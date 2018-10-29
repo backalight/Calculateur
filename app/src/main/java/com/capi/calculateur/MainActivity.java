272ae181609471e24227462ee9be41b4a81c025e
backaligth <antoine.soros@orange.fr>
2018-10-29 11:22:14 +0100

    skin calculette

--- app/src/main/java/com/capi/calculateur/MainActivity.java
+++ app/src/main/java/com/capi/calculateur/MainActivity.java
@@ -2,12 +2,16 @@ package com.capi.calculateur;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView ecran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ecran=findViewById(R.id.tvResult);
+        ecran.setText("0");
     }
 }
